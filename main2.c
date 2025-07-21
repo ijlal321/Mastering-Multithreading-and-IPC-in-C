2a6be013ddf7f7d3f1693906fb4f0c4c0cbc423e
ijlal321 <ijlal.tanveer294@gmail.com>
2025-07-21 17:22:38 +0500

    added dummy code

---
+++ main2.c
@@ -0,0 +1,117 @@
+#include <stdio.h>
+
+int dummy_function1(int a) { return a + 1; }
+int dummy_function2(int a) { return a + 2; }
+int dummy_function3(int a) { return a + 3; }
+int dummy_function4(int a) { return a + 4; }
+int dummy_function5(int a) { return a + 5; }
+int dummy_function6(int a) { return a + 6; }
+int dummy_function7(int a) { return a + 7; }
+int dummy_function8(int a) { return a + 8; }
+int dummy_function9(int a) { return a + 9; }
+int dummy_function10(int a) { return a + 10; }
+int dummy_function11(int a) { return a + 11; }
+int dummy_function12(int a) { return a + 12; }
+int dummy_function13(int a) { return a + 13; }
+int dummy_function14(int a) { return a + 14; }
+int dummy_function15(int a) { return a + 15; }
+int dummy_function16(int a) { return a + 16; }
+int dummy_function17(int a) { return a + 17; }
+int dummy_function18(int a) { return a + 18; }
+int dummy_function19(int a) { return a + 19; }
+int dummy_function20(int a) { return a + 20; }
+int dummy_function21(int a) { return a + 21; }
+int dummy_function22(int a) { return a + 22; }
+int dummy_function23(int a) { return a + 23; }
+int dummy_function24(int a) { return a + 24; }
+int dummy_function25(int a) { return a + 25; }
+int dummy_function26(int a) { return a + 26; }
+int dummy_function27(int a) { return a + 27; }
+int dummy_function28(int a) { return a + 28; }
+int dummy_function29(int a) { return a + 29; }
+int dummy_function30(int a) { return a + 30; }
+int dummy_function31(int a) { return a + 31; }
+int dummy_function32(int a) { return a + 32; }
+int dummy_function33(int a) { return a + 33; }
+int dummy_function34(int a) { return a + 34; }
+int dummy_function35(int a) { return a + 35; }
+int dummy_function36(int a) { return a + 36; }
+int dummy_function37(int a) { return a + 37; }
+int dummy_function38(int a) { return a + 38; }
+int dummy_function39(int a) { return a + 39; }
+int dummy_function40(int a) { return a + 40; }
+int dummy_function41(int a) { return a + 41; }
+int dummy_function42(int a) { return a + 42; }
+int dummy_function43(int a) { return a + 43; }
+int dummy_function44(int a) { return a + 44; }
+int dummy_function45(int a) { return a + 45; }
+int dummy_function46(int a) { return a + 46; }
+int dummy_function47(int a) { return a + 47; }
+int dummy_function48(int a) { return a + 48; }
+int dummy_function49(int a) { return a + 49; }
+int dummy_function50(int a) { return a + 50; }
+
+void print_dummy_message(int n) {
+    printf("This is dummy message number %d\n", n);
+}
+
+int main() {
+    int sum = 0;
+    sum += dummy_function1(1);
+    sum += dummy_function2(2);
+    sum += dummy_function3(3);
+    sum += dummy_function4(4);
+    sum += dummy_function5(5);
+    sum += dummy_function6(6);
+    sum += dummy_function7(7);
+    sum += dummy_function8(8);
+    sum += dummy_function9(9);
+    sum += dummy_function10(10);
+    sum += dummy_function11(11);
+    sum += dummy_function12(12);
+    sum += dummy_function13(13);
+    sum += dummy_function14(14);
+    sum += dummy_function15(15);
+    sum += dummy_function16(16);
+    sum += dummy_function17(17);
+    sum += dummy_function18(18);
+    sum += dummy_function19(19);
+    sum += dummy_function20(20);
+    sum += dummy_function21(21);
+    sum += dummy_function22(22);
+    sum += dummy_function23(23);
+    sum += dummy_function24(24);
+    sum += dummy_function25(25);
+    sum += dummy_function26(26);
+    sum += dummy_function27(27);
+    sum += dummy_function28(28);
+    sum += dummy_function29(29);
+    sum += dummy_function30(30);
+    sum += dummy_function31(31);
+    sum += dummy_function32(32);
+    sum += dummy_function33(33);
+    sum += dummy_function34(34);
+    sum += dummy_function35(35);
+    sum += dummy_function36(36);
+    sum += dummy_function37(37);
+    sum += dummy_function38(38);
+    sum += dummy_function39(39);
+    sum += dummy_function40(40);
+    sum += dummy_function41(41);
+    sum += dummy_function42(42);
+    sum += dummy_function43(43);
+    sum += dummy_function44(44);
+    sum += dummy_function45(45);
+    sum += dummy_function46(46);
+    sum += dummy_function47(47);
+    sum += dummy_function48(48);
+    sum += dummy_function49(49);
+    sum += dummy_function50(50);
+
+    for (int i = 1; i <= 50; ++i) {
+        print_dummy_message(i);
+    }
+
+    printf("Total sum: %d\n", sum);
+    return 0;
+}
